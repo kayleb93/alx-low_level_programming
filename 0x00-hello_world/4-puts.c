77d1e6d026f3f1fc27c27f0305b60d5e7919c159
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-09 14:43:53 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
+
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
-*
-*Description: using the main function
-*this program prints "Programming is very interesting"
-*Return: [2~[2~0
-*/
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
-{
-        puts("\Programming is very interesting"")[);
-        return (0);
+ {
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+[A[A[A[C{[B[B[B[B}
