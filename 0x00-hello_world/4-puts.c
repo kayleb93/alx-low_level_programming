a235500d21b4a3cedcb7841ec434ff43c065231a
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-09 14:25:28 +0200

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+*this program prints "Programming is very interesting"
+*Return: [2~[2~0
+*/
+int main(void)
+{
+        puts("\Programming is very interesting"")[);
+        return (0);
