4da8ba8ff980b8f5cbd1b78ead8dedcf14317222
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-11 15:15:05 +0200

    Write a C program that prints the size of various types on the computer it is compiled and run on

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Prints the sizes of many var types
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+     char a;
+     int b;
+     long int c;
+     long long int d;
+     float c;
+
+     printf("Size of a char: %d byte(s)\n", sizeof(a));
+     printf("Size of a char: %d byte(s)\n", sizeof(a));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dn int[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[b[C[C[C[C
+      printf("Size of an int: %d byte(s)\n", sizeof(b));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cprintf("Size of an int: %d byte(s)\n", sizeof(b))[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D long int: %d   byte([C)\n", sizeof(c));
+     printf("Size of an long int: %d   byte(s)\n", sizeof(c));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D long long int:  %d  byte(s)\n", sizeof(c));[D[D[d[C[C[C
+     printf("Size of a char: %d byte(s)\n", sizeof(a));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[float: %d byte(s)\n", sizeof(a));[D[D[c[C[C[C
+     return (0);
+}
