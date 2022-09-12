79cbef8365c35ecb7316d0d38319ce1fd6e37c1c
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-12 16:35:37 +0200

    alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- *main - print if the number is positive, zero, or negative
- *
- *Description: using main function
- *this program prints "Programming is positive, zero, or negative"
- *Return: 0
- */
- int main(void)
+*main - print if the number is positive, zero, or negative
+*
+*Description: using main function
+*this program prints "Programming is positive, zero, or negative"
+*Return: 0
+*/
+int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
