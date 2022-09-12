029ddd6b0197e08d2cce0b129ed5c1e5d30881f1
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-13 01:57:05 +0200

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	printf("Size of a char: %d byte(6)\n", sizeof(a));
-	printf("Size of an int: %[A[C[C[C[C[C[C[C[s[B[D[D[D[D[D[Dbyte(s)\n", sizeof(a));[D[D[b[C[C[C
-	printf("Size of a long int[A[Dd[B:[A[A[C[C[C[C[C[C[)[B[B[D[D[D[D[D[D%d byte(s)\n", sizeof(b));[D[D[c[B[C[C[C
-	printf("Size of a long long int: %d byte(s)\n", sizeof(c));[D[D[d[C[C[C
-	printf("Size of a float: %d byte(s)\n", sizeof(d));[D[D[e[C[C[C
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
 	return (0);
 }
