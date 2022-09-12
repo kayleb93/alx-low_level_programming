b9497ceb6972f584f2fe571818665eb6bf5dfbf6
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-13 01:45:25 +0200

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- *main - Prints the sizes of many var types
- *Return: Always 0 (Success)
- */
+*main - Printing
+*
+*Return: 0
+*/
 int main(void)
 {
-     char a;
-     int b;
-     long int c;
-     long long int d;
-     float c;
-
-     printf("Size of a char: %d byte(s)\n", sizeof(a));
-     printf("Size of a char: %d byte(s)\n", sizeof(a));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dn int[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[b[C[C[C[C
-      printf("Size of an int: %d byte(s)\n", sizeof(b));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cprintf("Size of an int: %d byte(s)\n", sizeof(b))[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D long int: %d   byte([C)\n", sizeof(c));
-     printf("Size of an long int: %d   byte(s)\n", sizeof(c));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D long long int:  %d  byte(s)\n", sizeof(c));[D[D[d[C[C[C
-     printf("Size of a char: %d byte(s)\n", sizeof(a));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[float: %d byte(s)\n", sizeof(a));[D[D[c[C[C[C
-     return (0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %d byte(6)\n", sizeof(a));
+	printf("Size of an int: %[A[C[C[C[C[C[C[C[s[B[D[D[D[D[D[Dbyte(s)\n", sizeof(a));[D[D[b[C[C[C
+	printf("Size of a long int[A[Dd[B:[A[A[C[C[C[C[C[C[)[B[B[D[D[D[D[D[D%d byte(s)\n", sizeof(b));[D[D[c[B[C[C[C
+	printf("Size of a long long int: %d byte(s)\n", sizeof(c));[D[D[d[C[C[C
+	printf("Size of a float: %d byte(s)\n", sizeof(d));[D[D[e[C[C[C
+	return (0);
 }
