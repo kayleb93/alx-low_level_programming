176c381670a6b384af7a05dc4727b87a2d4c146e
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-13 21:34:49 +0200

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,18 @@
 #include "main.h"
-
 /**
 *main - print putchar
 *Return: always 0
 */
-
 int main(void)
 {
-	_putcha('_');
-	_putcha('p');
-	_putcha('u');
-	_putcha('u');[D[D[t[C[C[C
-	_putcha('t');[D[D[c[C[C[C
-	_putcha('c');[D[D[h[C[C[C
-	_putcha('h');[D[D[a[C[C[C
-	_putcha('a');[D[D[D[D[D[C[r[D[D[Dr[A[Dr([D[B(''[Dr');
-	_putchar('\n');[A[A[A[D[D[D[D[D[D[D[Dr(''[Dh');[A[D[D[D[D[D[D[Dr(''[Dc');[B[B[D[D[D[D[D''[Da');[A[A[A[D[D[D[D[D[D[Dr('t');[A[D[D[D[D[D[D[Dr('u');[A[D[D[D[D[D[D[Dr('p'
-
-[C[C[C[C[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C);[A[D[D[D[D[D[D[Dr('_');[B[B[B[B[B[B[B[B[C
-
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 	return (0);
-
 }
