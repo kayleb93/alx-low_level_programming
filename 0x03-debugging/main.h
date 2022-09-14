17619256e1899eda13c4099f7f477b6c25c62a5f
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-14 13:18:27 +0200

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,11 @@
-#ifindef MAIN_H
-#define MAIN_H
+#ifndef MAIN.H
+#define MAIN.H
 
-#include <stdio.h>
+include <stdio.h>
+
 void positive_or_negative(int i);
-#endf
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /*[A[A[A[A[A[A[A[A[A[A[C[C[C[_[_    [B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[DMAIN_H */
