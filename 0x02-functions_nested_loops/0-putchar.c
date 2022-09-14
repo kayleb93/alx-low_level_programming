e5c11bf91cddba751e3dbe2e4d4cff20bc8ff1ee
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-15 00:48:13 +0200

    git commit -m '0-putchar.c'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *main - print putchar
 *Return: always 0
@@ -15,5 +16,6 @@ int main(void)
         _putchar('a');
         _putchar('r');
         _putchar('\n');
-        return (0);
+        
+	return (0);
 }
