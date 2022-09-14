c08ef0a742d510703458ed6c3fed90b5c3fb9550
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-14 14:06:59 +0200

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,5 @@
-#include "main.h"
-
 /**
- *largest_number[D[D[D[D[D[D[D[D[D[D[D[D[D[D [C[C[C[C[C[C[C[C[C[C[C[C[C - return[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dl[C[C[C[C[C[C[C[C[C[C[C[ * largest_number - returns the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -11,19 +9,17 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+if (a >= b && a >= c)
+{
+largest =a;
 }
+else if (b > a && a > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (largest);
+}[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C= a && b >= c)[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[B[B[B[D
