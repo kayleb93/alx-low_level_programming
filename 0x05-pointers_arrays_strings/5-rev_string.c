d9a5224e4ae61cad04f23d6d46fd66db0c80f2a2
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-19 18:30:53 +0200

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,6 +5,15 @@
  *
  * Return: void.
  */
+int main(void)
+{
+    char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
 void rev_string(char *s)
 {
 	int i, j, k, tmp;
