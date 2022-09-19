faeab13b32fd7976739c1ac9287c099f94cdc4aa
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-19 14:47:08 +0200

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+[1;5s#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts - prints every other character
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(*(str + i))
+		i++;
+	}
+	putchar(10);
+}
