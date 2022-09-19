4276141534d186e64af16a6e170b59909883ab31
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-19 14:14:38 +0200

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,9 +3,9 @@
 #include "main.h"
 
 /** 
-*print_rev - Prints a string in reverse order
-*@s: String to reverse
-*Return: Nothing
+* print_rev - Prints a string in reverse order
+* @s: String to reverse
+* Return: Nothing
 */
 
 void print_rev(char *s)
