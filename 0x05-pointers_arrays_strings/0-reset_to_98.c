b5f5c81284b2a0b1bdd4572922c00bd5dfbfb21e
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-19 11:40:54 +0200

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/*
+*
+*
+*/
+void reset_to_98(int *n)[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[D[Areset_to_98 -> a function to update the reference of a pointer
+[B[B[B[B[B[B* @n: pointer to n to be updated
+*/
+void reset_to_98(int *n)
+{
+	int x = 1;
+[A[C[C[C[C[C[C[C[*n = 98;[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif[A[A[C[C[C[C[C[C[C[C
+int _putchar(char c);[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[C[C[C[C[C[C
