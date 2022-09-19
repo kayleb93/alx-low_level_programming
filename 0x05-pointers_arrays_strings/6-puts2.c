4f91fa3d4413293bf2ac9743ea1f38d4afc020af
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-19 18:57:18 +0200

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,14 +12,14 @@ void puts2(char *str)
 {
 	int i;
 
-	int = 0;
+	int() = 0;
 	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 		}
 		i++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
