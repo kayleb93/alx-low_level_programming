53f08cf57720e83f6dcaad22bcb93584d3c7b3ff
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-23 11:57:21 +0200

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * rot13 - rot13 encoder
+ * @str: string to be encoded
+ *
+ * Return: address of the encoded string
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform_2(str[i]);
+		i++;
+	}
+	return (str);
+}
+
+#include "main.h"
+
+/**
  * rot13 - encodes a string into rot13
  * @str: The string to encode
  *
