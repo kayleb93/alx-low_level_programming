a6e773bed2274016b1064d5b97305061b8460dff
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-26 23:41:30 +0200

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -19,9 +19,9 @@
 void *_memset(void *s, int c, size_t n)
 {
 	unsigned int index;
-	unsigned char *memory = s; value = c;
+	unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
+	for (index = 0; index < n; index++;)
 		memory[index] = value;
 
 	return (memory);
