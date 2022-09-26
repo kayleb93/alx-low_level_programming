faa774903753b16d8ac7e53870bd7a596cfe64b2
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-26 16:51:26 +0200

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,9 +2,13 @@
 #define MAIN_H
 
 /**
-* void prototypes()
-* int prototypes()
-*/
+ * File: main.h
+ * Auth: sam tech (samuel birhanu)
+ * description: Header file containing prototypes for all functions
+ * 	used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
 
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
