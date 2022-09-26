6285860f042972fe3da4d5666d7b52290b01c293
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-26 17:17:11 +0200

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,15 +14,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int index;
 
-	for (i = 0; *(s + i); i++)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
-		if (*(s + i) == c)
-			return (s + i);
+		if (s[index] == c)
+			return (s + index);
 	}
-	if (*(s + i) == c)
-		return (s + i);
-	
-	return (0);
+
+	return ('\0');
 }
