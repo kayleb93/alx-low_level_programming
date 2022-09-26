360380cd434bfcbe5462ebc286b983f681e41d0e
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-26 23:54:56 +0200

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -21,7 +21,7 @@ void *_memcpy(void *dest, const void *src, size_t n)
 	unsigned char *destination = dest;
 	const unsigned char *source = src;
 	
-	for (index = 0; index < n; index++)
+	for (; index < n; index++)
 	{
 		destination[index] = source[index];
 	}
