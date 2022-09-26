75d0aa981f467e196a92ba30a38ce129fec1ccb9
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-26 18:16:02 +0200

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,15 +11,15 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int i, j;
 	
-	for (i = 0; s[i]; i++)
+	for(i = 0; s[i]; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for(j = 0; accept[j]; j++)
 		{
-			if (s[i] == accept[i])
+			if(s[i] == accept[i])
 				break;
 		}
-		if (accept[j])
-			return (s + i);
+		if(accept[j])
+			return(s + i);
 	}
-	return (0);
+	return(0);
 }
