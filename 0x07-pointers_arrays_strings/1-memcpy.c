a6a528c93f97a83a36d9ff1d576f58bc47df02af
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-27 22:00:32 +0200

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,3 @@
- /*
- * File: 0-memset.c
- * Auth: not
- */
-
 #include "main.h"
 
 /**
@@ -14,10 +9,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int a;
+	unsigned int i;
 	
-	for (a = 0; a < n; a++)
-		dest[a] = src[a];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	
 	return (dest);
 }
