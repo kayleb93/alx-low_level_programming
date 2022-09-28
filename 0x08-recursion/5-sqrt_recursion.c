94e07bad8d87ef108db3ea562c8c280ec1d73990
kayleb93 <kelebogiletshupo@gmail.com>
2022-09-28 23:24:47 +0200

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -26,12 +26,11 @@ return (-1);
  *   *@n: integer
  *    *Return: sqrt_recursion
  *    */
-
 int _sqrt_recursion(int n)
 {
 	int i = 0;
 
-	if ( i < 0) /*If n is negative*/
+	if (i < 0) /*If n is negative*/
 		return (-1);
 
 	else
