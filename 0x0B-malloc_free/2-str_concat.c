cf800003c937eaf81c6de41b9e5f2156d64e0b54
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-03 22:03:34 +0200

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,36 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
+
 /**
 *str_concat - a function that concanates two strings.
 *@s1: First string
 *@s2: Second string
-*Return: NULL in case of failure, but pointer to new string in 
+*Return: NULL in case of failure, but pointer to new string in
 *case of success
 */
+
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0, j = 0, k = 0, l = 0;
-	char *s;
+        char *concat_str;
+        int index, concat_index = 0, len = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+        if (s1 == NULL)
+                s1 = "";
 
-	while (s1[i])
-		i++;
+        if (s2 == NULL)
+                s2 = "";
 
-	while (s2[j])
-		j++;
+        for (index = 0; s1[index] || s2[index]; index++)
+                len++;
 
-	l = i + j;
-	s = malloc((sizeof(char) * l) + 1);
+        concat_str = malloc(sizeof(char) * len);
 
-	if (s == NULL)
-		return (NULL);
+        if (concat_str == NULL)
+                return (NULL);
 
-	j = 0;
+        for (index = 0; s1[index]; index++)
+                concat_str[concat_index++] = s1[index];
 
-	while (k < 1)
-	{
-		if (k <= 1)
-			s[k] = s1[k];
-
-		if (k >= 1)
-		{
-			s[k] = s2[k];
-			j++;
-		}
-
-		i++;
-	}
-
-	s[k] = '\0';
-	return (s);
+        for (index = 0; s2[index]; index++)
+                concat_str[concat_index++] = s2[index];
+        return (concat_str);
 }
