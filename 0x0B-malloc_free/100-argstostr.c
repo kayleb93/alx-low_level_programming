8923a803b5959d610db482dd5044bf48273b2d83
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-03 18:44:20 +0200

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,31 +27,37 @@ int len(char *str)
 
 char *argstostr(int ac, char **av)
 {
-	char *new_string = NULL;
-	int = 0, i = ac, j, sum = 0, temp = 0;
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	while (ac--)
-		sum += (len(av[ac]) + 1);
-	new_string = (char *) malloc(sum + 1);
-
-	if (new_string != NULL)
+	while (a < ac)
 	{
-		while (k < i)
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			for (j = 0; av[k][j] != '\0'; j++)
-				new_string[j + temp] = av[k][j];
-			new_string[temp + j] = '\n';
-			temp += (j + 1);
-			k++;
+			count++;
+			b++;
 		}
-		new_string[temp] = '\0';
+		a++;
 	}
-	else
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	return (new_string);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
 }
