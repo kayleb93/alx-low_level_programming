fbb67602ff830db8834c3b335a217ef9800bbe3b
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-06 17:12:21 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,13 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int size1 = 0, size2 = 0, 1;
+	unsigned int size1 = 0, size2 = 0, i;
 
 	if (s1 == NULL)
-		s1 = '';
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = '';
+		s2 = "";
 
 	while (s1[size1] != '\0')
 	{
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n > size2)
 	n = size2;
-	p = malloc((size1 + n + l) * sizeof(char));
+	p = malloc((size1 + n + 1) * sizeof(char));
 
 	if (p == NULL)
 		return (0);
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
+
