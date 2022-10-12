45f9b120eb37108c1adcd5f04e46390931a44a05
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-12 16:50:10 +0200

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,20 +1,9 @@
 #include "3-calc.h"
-
 /**
- * get_op_func - function pointer that selects the correct function to perform
- * the operation asked by the user
- * @s: the operator given by the user
+ * get_op_func - function pointer that selects the correct function to perform.
+ * @s: the operator used.
  *
- * Return: pointer to the function that corresponds to the 
- * operator given a#include "3-calc.h"
-
-/**
- * get_op_func - function pointer that selects the correct function to perform
- * the operation asked by the user
- * @s: the operator given by the user
- *
- * Return: pointer to the function that corresponds to the
- * operator given as a parameter
+ * Return: correct function result or NULL if operator is wrong.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -29,14 +18,14 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
-	while (ops[i].op)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (s[0] == ops[i].op[0])
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
-
 	return (NULL);
 }
 ~
