5fa2c5be6167d53fdd1dff7144a4f5f34162e482
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-12 14:26:28 +0200

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,6 +6,7 @@
  * @array: array to search in 
  * @size: size of the array
  * @cmp: pointer to comparing function
+ *
  * Return: index of the first element for which
  * the cmp function does not return 0, or -1 if no match is found
  * or size is negative
