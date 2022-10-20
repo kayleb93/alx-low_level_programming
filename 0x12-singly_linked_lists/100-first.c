44604d877e9c85022d720378ecdc880df8a24bae
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-20 21:14:19 +0200

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void __attribute__ ((constructor)) first()
 {
-	printf("you're beat! and yet, you must allow");
+	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
