ec0ea3649f03014577af147878eaa396fff9cd7c
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-26 10:06:22 +0200

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * add_nodeint - Adds a new node at the beginning
- * 		 of a listint_t list.
+ * of a listint_t list.
  * @h: A pointer to the address of the
- * 	  head of the listint_t list.
+ * head of the listint_t list.
  *
  * Return: The number of elements in the listint_t list.
- * 	    Otherwise - the address of the new element.
+ * Otherwise - the address of the new element.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
