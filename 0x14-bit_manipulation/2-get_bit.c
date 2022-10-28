3ad42876e4720789422b6408797c372966829976
kayleb93 <kelebogiletshupo@gmail.com>
2022-10-28 10:28:28 +0200

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,9 +15,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == && index < 64)
 		return (0);
 
-	for (i = 0; i <= 63; n >= 1, i++)
+	for (i = 0; i <= 63; n >>= 1, i++)
 	{
-		if (index == 1)
+		if (index == i)
 		{
 			return (n & 1);
 		}
