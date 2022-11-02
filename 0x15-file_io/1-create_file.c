2b9a3acce0f61e53bea9461c07ad29745463fa8e
kayleb93 <kelebogiletshupo@gmail.com>
2022-11-02 17:42:57 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- * 	Otherwise - 1
+ * Otherwise - 1
  */
 
 int create_file(const char *filename, char *text_content)
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_CREAT | O_ROWR | O_TRUNC, 0000);
+	o = open(filename, O_CREAT | O_ROWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
