52fe5d918f1a6ceec615bce03eb1d8dd3cb842a0
kayleb93 <kelebogiletshupo@gmail.com>
2022-11-04 19:52:19 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,36 +11,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	if (fd == -1)
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
-
-	close(fd);
+	close(o);
 
 	return (1);
-}
-
-/**
- * _strlen - length of string
- * @s: String to count
- *
- * Return: String length
- */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
 }
